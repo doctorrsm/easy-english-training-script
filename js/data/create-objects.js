2416308b2c6fa02f79ef9cea7945d4d6469f9bcc
Sergey Rutkovsky <dr.bibelman@gmail.com>
2022-03-30 12:15:56 +0300

    Добавляет урок тренажёр номер 1

--- js/data/create-objects.js
+++ js/data/create-objects.js
@@ -1,5 +1,6 @@
 
 const sentences = document.querySelector('.sentences');
+console.log(sentences.innerHTML)
 const arr = sentences.innerHTML.split('\n');
 sentences.style.display = 'none';
 const objectss = [];
@@ -18,7 +19,7 @@ const createObjects = () => {
 
 createObjects();
 
-console.log(JSON.stringify(objectss));
+console.log(objectss);
 
 export {objectss };
 
